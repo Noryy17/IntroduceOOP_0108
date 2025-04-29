2227459920e7fcddb72e095086329b64e49feb32
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-04-29 18:31:50 +0700

    Membuat Procedure PrintData

--- OOP.cpp
+++ OOP.cpp
@@ -6,10 +6,13 @@ class Mahasiswa{
     int nim;
     string nama;
     float nilai;
-    
 
- 
+    void printdata (){
+        cout << "Masukkan Nim : " << nim << endl;
+        cout << "Masukkan Nama : " << nama << endl;
+        cout << "Masukkan Nilai :" << nilai << endl;
+    }
 };
 int main(){
-    
+   
 }
