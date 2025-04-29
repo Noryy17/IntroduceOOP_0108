7baae2d2b4b65521799a543170681d79013bb64f
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-04-29 18:19:37 +0700

    Membuat Class Mahasiswa

--- OOP.cpp
+++ OOP.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+class Mahasiswa{
+    public:
+    int nim;
+    string nama;
+    float nilai;
+
+    void printdata (){
+        cout << "Masukkan Nim : " << nim << endl;
+        cout << "Masukkan Nama : " << nama << endl;
+        cout << "Masukkan Nilai : " << nilai << endl;
+    }
+};
+int main(){
+    Mahasiswa mhs;
+    mhs.nim = 2024;
+    mhs.nama = "Abra";
+    mhs.nilai = 87;
+
+    mhs.printdata();
+}
