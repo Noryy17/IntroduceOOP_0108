ba35de10511d2ddfa676fee0efd6d044f24b2e4c
Faid arya <faid.arya.ft24@mail.umy.ac.id>
2025-04-29 18:22:53 +0700

    Membuat Akses Modifier Public Agar bisa dibaca oleh bahasa pemrograman lain.

--- OOP.cpp
+++ OOP.cpp
@@ -7,17 +7,8 @@ class Mahasiswa{
     string nama;
     float nilai;
 
-    void printdata (){
-        cout << "Masukkan Nim : " << nim << endl;
-        cout << "Masukkan Nama : " << nama << endl;
-        cout << "Masukkan Nilai : " << nilai << endl;
-    }
+ 
 };
 int main(){
-    Mahasiswa mhs;
-    mhs.nim = 2024;
-    mhs.nama = "Abra";
-    mhs.nilai = 87;
-
-    mhs.printdata();
+    
 }
